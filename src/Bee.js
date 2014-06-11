516bc2431e2160472be47a7d578e07e9da67095c
Jasen Lew <jasen.lew@gmail.com>
2014-06-10 23:21:15 -0700

    Created Bee and HoneyMakerBee classes

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
-var Bee = function() {
+var Bee = function(age, color, food) {
+
+	Grub.call(this);  // QUESTION: CAN I PASS IN 5 AND YELLOW AS SEPARATE ARGUMENTS AFTER "this"?
+	
+	this.age = 5;
+	this.color = "yellow";
+	this.job = "keep on growing";
+
+	this.honeyPot = 0;
+
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
