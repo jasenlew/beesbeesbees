817d507b20c6cf01129240c9186dcd9d277a91d1
Jasen Lew <jasen.lew@gmail.com>
2014-06-11 07:48:03 -0700

    Add a comment question

--- src/Grub.js
+++ src/Grub.js
@@ -1,6 +1,6 @@
 var Grub = function() {
 
-	this.age = 0;
+	this.age = 0;		// QUESTION: IF I CAN PUT DEFAULT PROPERTIES IN PROTOTYPE, CAN I PUT THE 3 PROPERTIES HERE IN PROTOTYPE? 
 	this.color = "pink";
 	this.food = "jelly";
 
